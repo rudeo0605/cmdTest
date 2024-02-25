b1ee739eb06efbafa90d69ac5ea8c59464715076
park <rudeo7701@gmail.com>
2024-02-25 18:09:02 +0900

    코드 처리하기

--- cmdTest/main.swift
+++ cmdTest/main.swift
@@ -6,17 +6,47 @@
 //
 
 //
-// flag에 따라 다른 값 반환하기
-// flag가 true면 a + b
-// flag가 false면 a - b
+// 코드 처리하기
+// mode가 0이면 idx가 짝수일 때만 ret 맨 뒤에 code[idx]를 추가
+// 속도 문제
+// 시간 초과.. 시간 줄이는 방법 찾기
+// Swift의 String은 유니코드를 처리하기위해 만들져서 인덱스를 통한 접근은 비효율적
+// 문자열 인덱스 직접 계산 대신 enumerate() 사용
 
 import Foundation
 
-func solution(_ a:Int, _ b:Int, _ flag:Bool) -> Int {
+func solution(_ code:String) -> String {
     
-    return flag ? a + b : a - b
+    var mode = 0
+    
+    var result = ""
+    
+    let list = code.enumerated()
+    
+    // 인덱스, 값을 모두 얻을 수 있음.
+    for (idx, cha) in list
+    {
+        if(cha == "1")
+        {
+            mode = mode == 0 ? 1 : 0
+        }
+        else if(idx == 0)
+        {
+            result += String(cha)
+        }
+        else if(mode == 0 && idx % 2 == 0)
+        {
+            result += String(cha)
+        }
+        else if(mode == 1 && idx % 2 == 1)
+        {
+            result += String(cha)
+        }
+    }
+    
+    return result == "" ? "EMPTY" : result
 }
 
-var result = solution(-4, 7, true)
+var result = solution("abc1abc1abc")
 
 print(result)
