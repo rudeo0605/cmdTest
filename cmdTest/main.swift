8afde9cb6e5e8569468d4036410d1ba2f06824e9
park <rudeo7701@gmail.com>
2024-02-25 18:27:58 +0900

    등차수열의 특정한 항만 더하기

--- cmdTest/main.swift
+++ cmdTest/main.swift
@@ -6,47 +6,30 @@
 //
 
 //
-// 코드 처리하기
-// mode가 0이면 idx가 짝수일 때만 ret 맨 뒤에 code[idx]를 추가
-// 속도 문제
-// 시간 초과.. 시간 줄이는 방법 찾기
-// Swift의 String은 유니코드를 처리하기위해 만들져서 인덱스를 통한 접근은 비효율적
-// 문자열 인덱스 직접 계산 대신 enumerate() 사용
+// 등차수열의 특정한 항만 더하기
+//
 
 import Foundation
 
-func solution(_ code:String) -> String {
+func solution(_ a:Int, _ d:Int, _ included:[Bool]) -> Int {
     
-    var mode = 0
+    var result = 0
     
-    var result = ""
     
-    let list = code.enumerated()
+    var sum = a;
     
-    // 인덱스, 값을 모두 얻을 수 있음.
-    for (idx, cha) in list
+    for (_, isBool) in included.enumerated()
     {
-        if(cha == "1")
+        if(isBool)
         {
-            mode = mode == 0 ? 1 : 0
+            result += sum
         }
-        else if(idx == 0)
-        {
-            result += String(cha)
-        }
-        else if(mode == 0 && idx % 2 == 0)
-        {
-            result += String(cha)
-        }
-        else if(mode == 1 && idx % 2 == 1)
-        {
-            result += String(cha)
-        }
+        sum += d
     }
     
-    return result == "" ? "EMPTY" : result
+    return result
 }
 
-var result = solution("abc1abc1abc")
+var result = solution(3, 4, [true, false, false, true, true])
 
 print(result)
