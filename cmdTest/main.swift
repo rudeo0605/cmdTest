912d577fefcaf7fe891eb4199a4c5c4868a3c1db
park <rudeo7701@gmail.com>
2024-02-25 18:53:22 +0900

    주사위 게임 2

--- cmdTest/main.swift
+++ cmdTest/main.swift
@@ -6,30 +6,41 @@
 //
 
 //
-// 등차수열의 특정한 항만 더하기
-//
+// 주사위 게임 2
+// 주사위 3개 조건
+// 1. 모두 같은 경우
+// 2. 모두 다른 경우
+// 3. 2개만 같은 경우
 
 import Foundation
 
-func solution(_ a:Int, _ d:Int, _ included:[Bool]) -> Int {
+func solution(_ a:Int, _ b:Int, _ c:Int) -> Int {
     
-    var result = 0
+    let a1 = Double(a)
+    let b1 = Double(b)
+    let c1 = Double(c)
     
+    var result:Int = 0
     
-    var sum = a;
-    
-    for (_, isBool) in included.enumerated()
+    if(a == b && a == c)
     {
-        if(isBool)
-        {
-            result += sum
-        }
-        sum += d
+        result = 
+        (a + b + c)
+        * Int( pow(a1, 2) + pow(b1, 2) + pow(c1, 2) )
+        * Int( pow(a1, 3) + pow(b1, 3) + pow(c1, 3) )
     }
-    
+    else if(a != b && a != c && b != c)
+    {
+        result = a + b + c
+    }
+    else
+    {
+        result = (a + b + c) * Int( pow(a1,2) + pow(b1, 2) + pow(c1, 2))
+    }
+
     return result
 }
 
-var result = solution(3, 4, [true, false, false, true, true])
+var result = solution(5,3,3)
 
 print(result)
