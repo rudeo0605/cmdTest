beb0f2d8b23f42a40b72efa9a487ad63cf29f23b
park <rudeo7701@gmail.com>
2024-02-25 21:25:10 +0900

    카운트 업

--- cmdTest/main.swift
+++ cmdTest/main.swift
@@ -6,46 +6,24 @@
 //
 
 //
-// 배열 만들기 2
-// l 이상 r 이하 정수 중 숫자 0 과 5 로만 이루어진 모든 정수를 오름차순으로 만들기
-// 5의 배수만 확인
-// String으로 변환한 후 "5", "0" 개수 체크
-// String 길이와 비교하여 같으면 리스트에 추가
-// 리스트가 비어있으면 -1 추가
+// 카운트 업
+// startNum, endNum
+//
 
 import Foundation
 
-func solution(_ l:Int, _ r:Int) -> [Int] {
+func solution(_ start_num:Int, _ end_num:Int) -> [Int] {
     
     var result : [Int] = []
     
-    for i in l...r
+    for num in start_num...end_num
     {
-        if(i % 5 == 0)
-        {
-            var sum = 0;
-            
-            let tempString = String(i)
-            let tempStringLength = tempString.count
-            
-            sum += tempString.filter{$0 == "5"}.count
-            sum += tempString.filter{$0 == "0"}.count
-            
-            if(sum == tempStringLength)
-            {
-                result.append(i)
-            }
-        }
-    }
-    
-    if(result.isEmpty)
-    {
-        result.append(-1)
+        result.append(num)
     }
     
     return result
 }
 
-var result = solution(5, 555)
+var result = solution(3, 10)
 
 print(result)
