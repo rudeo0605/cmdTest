098a200626f4b7ef375d4e08fd924aff9803414c
park <rudeo7701@gmail.com>
2024-02-25 19:32:05 +0900

    이어 붙인 수

--- cmdTest/main.swift
+++ cmdTest/main.swift
@@ -6,24 +6,29 @@
 //
 
 //
-// 원소들의 곱과 합
+// 이어 붙인 수
 //
 
 import Foundation
 
 func solution(_ num_list:[Int]) -> Int {
     
-    var sum1 = 0
-    var sum2 = 0
-
-    // reduce를 사용하는 이유 - 속도
-    // for-in 을 사용하면 목록을 하나씩 반복해야함
-    // reduce를 사용하면 한번에 처리
-    //
-    sum1 = num_list.reduce(1, *)
-    sum2 = Int(pow(Double(num_list.reduce(0, +)), 2))
+    var a = ""
+    var b = ""
     
-    return sum1 >= sum2 ? 0 : 1
+    for num in num_list
+    {
+        if(num % 2 == 0)
+        {
+            b += String(num)
+        }
+        else
+        {
+            a += String(num)
+        }
+    }
+    
+    return (Int(a)! + Int(b)!)
 }
 
 var result = solution([3, 4, 5, 2, 1])
