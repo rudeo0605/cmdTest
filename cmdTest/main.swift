f4c45c29166dbd766b3362bfc82dddcd740fb91b
park <rudeo7701@gmail.com>
2024-02-25 20:33:30 +0900

    수열과 구간 쿼리2

--- cmdTest/main.swift
+++ cmdTest/main.swift
@@ -6,26 +6,51 @@
 //
 
 //
-// 수열과 구간 쿼리 3
-//
+// 수열과 구간 쿼리 2
+// query = s, e, k
+// arr 를 s ~ e 까지 잘라 newArr 만들기
+// newArr의 값들을  k와 비교하여 k 보다 크면서 가장 작은 수를 result에 저장
+// 만약 조건에 맞는 값이 없을 경우 -1을 추가
 
 import Foundation
 
 func solution(_ arr:[Int], _ queries:[[Int]]) -> [Int] {
     
-    var list = arr
+    var result:[Int] = []
     
-    for (_,querie) in queries.enumerated()
+    for (_, query) in queries.enumerated()
     {
-        var temp = list[querie[0]]
+        let s = query[0]
+        let e = query[1]
+        let k = query[2]
         
-        list[querie[0]] = list[querie[1]]
-        list[querie[1]] = temp
+        let sliceList:[Int] = Array(arr[s...e])
+        
+        var tempList:[Int] = []
+        
+        for num in sliceList
+        {
+            if(num > k)
+            {
+                tempList.append(num)
+            }
+        }
+        
+        tempList.sort(by: <)
+        
+        if(tempList.isEmpty)
+        {
+            result.append(-1)
+        }
+        else
+        {
+            result.append(tempList[0])
+        }
     }
     
-    return list
+    return result
 }
 
-var result = solution([0, 1, 2, 3, 4], [[0, 3],[1, 2],[1, 4]])
+var result = solution([0, 1, 2, 4, 3], [[0, 4, 2],[0, 3, 2],[0, 2, 2]])
 
 print(result)
