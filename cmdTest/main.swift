3ad802192a9d1b700cfaec7e6161683d41458a2e
park <rudeo7701@gmail.com>
2024-02-25 17:11:39 +0900

    n의 배수

--- cmdTest/main.swift
+++ cmdTest/main.swift
@@ -6,27 +6,18 @@
 //
 
 //
-// 두 수의 연산값 비교하기
-// n1, n2 가 주어졌을때 n1 ⊕ n2 또는 2 * n1 * n2 중 더 큰 수를 리턴해라
-// 1 <= arr <= 10,000
+// n의 배수
+// num이 n의 배수이면 1 배수가 아니라면 0
 //
 
 import Foundation
 
-func solution(_ a:Int, _ b:Int) -> Int {
+func solution(_ num:Int, _ n:Int) -> Int {
     
-    var value1 = 0;
-    var value2 = 0;
     
-    let temp1 = String(a) + String(b)
-    
-    value1 = Int(temp1)!
-
-    value2 = 2 * a * b
-    
-    return value1 <= value2 ? value2 : value1
+    return num % n == 0 ? 1 : 0
 }
 
-var result = solution(91,2)
+var result = solution(34,3)
 
 print(result)
