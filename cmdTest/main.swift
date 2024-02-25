51fc4b3f1df6f5a9ac31dcb9d56eaedc8f4f6ca6
park <rudeo7701@gmail.com>
2024-02-25 20:00:19 +0900

    수열과 구간 쿼리 3

--- cmdTest/main.swift
+++ cmdTest/main.swift
@@ -6,51 +6,26 @@
 //
 
 //
-// 수 조작하기 2
-// 이전값 - 현재값을 해서 값을 구함
+// 수열과 구간 쿼리 3
 //
 
 import Foundation
 
-func solution(_ numLog:[Int]) -> String {
+func solution(_ arr:[Int], _ queries:[[Int]]) -> [Int] {
     
-    var previousNumber = 0
+    var list = arr
     
-    var result = ""
-    
-    for (idx, num) in numLog.enumerated()
+    for (_,querie) in queries.enumerated()
     {
-        if(idx == 0)
-        {
-            previousNumber = num
-        }
-        else
-        {
-            let value = num - previousNumber
-            if(value == 1)
-            {
-                result += "w"
-            }
-            else if(value == -1)
-            {
-                result += "s"
-            }
-            else if(value == 10)
-            {
-                result += "d"
-            }
-            else if(value == -10)
-            {
-                result += "a"
-            }
-            
-            previousNumber = num
-        }
+        var temp = list[querie[0]]
         
+        list[querie[0]] = list[querie[1]]
+        list[querie[1]] = temp
     }
-    return result
+    
+    return list
 }
 
-var result = solution([0, 1, 0, 10, 0, 1, 0, 10, 0, -1, -2, -1])
+var result = solution([0, 1, 2, 3, 4], [[0, 3],[1, 2],[1, 4]])
 
 print(result)
