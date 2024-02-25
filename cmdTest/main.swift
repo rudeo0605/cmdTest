a711dd3d39d8f2562f6caf521de492a247c11f96
park <rudeo7701@gmail.com>
2024-02-25 17:14:38 +0900

    공배수

--- cmdTest/main.swift
+++ cmdTest/main.swift
@@ -6,18 +6,16 @@
 //
 
 //
-// n의 배수
-// num이 n의 배수이면 1 배수가 아니라면 0
+// 공배수
+// num이 n의 배수이면서 m의 배수이면 1 배수가 아니라면 0
 //
 
 import Foundation
 
-func solution(_ num:Int, _ n:Int) -> Int {
-    
-    
-    return num % n == 0 ? 1 : 0
+func solution(_ number:Int, _ n:Int, _ m:Int) -> Int {
+    return number % n == 0 && number % m == 0 ? 1 : 0
 }
 
-var result = solution(34,3)
+var result = solution(55,10,5)
 
 print(result)
