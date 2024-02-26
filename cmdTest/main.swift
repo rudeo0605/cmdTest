c960349738a0379f342a79d23b38068d3f497524
KyungDae PARK <kyungdae.park@futurereality.io>
2024-02-26 12:45:53 +0900

    제곱수 판별하기

--- cmdTest/main.swift
+++ cmdTest/main.swift
@@ -6,36 +6,20 @@
 //
 
 //
-// 옷가게 할인 받기
+// 제곱수 판별하기
 
 import Foundation
 
-func solution(_ price:Int) -> Int {
+func solution(_ n:Int) -> Int {
 
-    var result = 0;
+    let sqrtN = Int(Double(n).squareRoot()) 
 
-    let dou = Double(price)
+    let powN = Int(pow(Double(sqrtN), 2))
 
-    if (price >= 500000)
-    {
-        result = Int(dou * 0.80)
-    }
-    else if(price >= 300000)
-    {
-        result = Int(dou * 0.90)
-    }
-    else if(price >= 100000)
-    {
-        result = Int(dou * 0.95)
-    }
-    else
-    {
-        result = price
-    }
-
-
-    return result
+    return n == powN ? 1 : 2
 }
-var result = solution(150000)
 
+var result = solution(144)
+var result1 = solution(976)
 print(result)
+print(result1)
