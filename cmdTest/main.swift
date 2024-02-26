3894738df2003a147ff91af581373d7851d14df7
KyungDae PARK <kyungdae.park@futurereality.io>
2024-02-26 11:31:04 +0900

    삼각형의 완성조건 (1)

--- cmdTest/main.swift
+++ cmdTest/main.swift
@@ -6,20 +6,22 @@
 //
 
 //
-// 세균 증식
+// 삼각형의 완성조건 (1)
 
 
 import Foundation
 
-func solution(_ n:Int, _ t:Int) -> Int {
+func solution(_ sides:[Int]) -> Int {
 
-    var result = n;
-    for _ in 0..<t
-    {
-        result *= 2
-    }
-    return result
+    var sortSides = sides
+    sortSides.sort(by: >)
+
+    let max = sortSides[0]
+    let sum = sortSides[1] + sortSides[2]
+
+    return max < sum ? 1 : 2
 }
-var result = solution(2,10)
+
+var result = solution([1, 2, 3])
 
 print(result)
