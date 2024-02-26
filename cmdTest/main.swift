c8c92206da3157b914bf1fc36032935bec5c88ce
KyungDae PARK <kyungdae.park@futurereality.io>
2024-02-26 12:21:12 +0900

    순서쌍의 개수

--- cmdTest/main.swift
+++ cmdTest/main.swift
@@ -14,11 +14,13 @@ import Foundation
 func solution(_ n:Int) -> Int {
     var result = 0
     
+    //제곱근을 넘어가면 J가 I보다 작아지기 시작
     let sqrtN = Int(Double(n).squareRoot()) // n의 제곱근을 구합니다.
     print(sqrtN)
     for i in 1...sqrtN {
         if n % i == 0 { // i가 n의 약수인 경우
             let j = n / i
+            print("\(n) / \(i) = \(JobPriority)")
             if i == j {
                 result += 1 // i와 j가 같으면, 즉 n이 완전제곱수인 경우
             } else {
