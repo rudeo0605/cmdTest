2bd87d781d9067a2edd77c5489f4808ba7178736
KyungDae PARK <kyungdae.park@futurereality.io>
2024-02-26 13:32:56 +0900

    n 번째 원소까지

--- cmdTest/main.swift
+++ cmdTest/main.swift
@@ -6,13 +6,13 @@
 //
 
 //
-// 정수 찾기
+// n 번째 원소까지
 import Foundation
 
-func solution(_ num_list:[Int], _ n:Int) -> Int {
-    return num_list.contains(n) ? 1 : 0
+func solution(_ num_list:[Int], _ n:Int) -> [Int] {
+    return Array(num_list[0..<n])
 }
 
-var result = solution([1, 2, 3, 4, 5], 3)
+var result = solution([2, 1, 6], 1)
 
 print(result)
