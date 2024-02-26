6720bfb0514f7bb631ade7bed42a076f3d029c98
KyungDae PARK <kyungdae.park@futurereality.io>
2024-02-26 14:15:00 +0900

    부분 문자열

--- cmdTest/main.swift
+++ cmdTest/main.swift
@@ -6,26 +6,13 @@
 //
 
 //
-// 배열 비교하기
+// 부분 문자열
 import Foundation
 
-func solution(_ arr1:[Int], _ arr2:[Int]) -> Int {
-    let aSize = arr1.count
-    let bSize = arr2.count
-
-    if(aSize == bSize)
-    {
-        let ac = arr1.reduce(0,{ $0 + $1 })
-        let bc = arr2.reduce(0,{ $0 + $1 })
-        
-        return ac == bc ? 0 : ac > bc ? 1 : -1
-    }
-    else
-    {
-        return aSize > bSize ? 1 : -1
-    }
+func solution(_ str1:String, _ str2:String) -> Int {
+    return str2.contains(str1) ? 1 : 0
 }
 
-var result = solution([100, 17, 84, 1]	, [55, 12, 65, 36]	)
+var result = solution("abc", "aabcc")
 
 print(result)
