7cc3007162397dad05c8d742fb2f92a80f681962
KyungDae PARK <kyungdae.park@futurereality.io>
2024-02-26 13:47:42 +0900

    조건에 맞게 수열 변환하기 3

--- cmdTest/main.swift
+++ cmdTest/main.swift
@@ -6,18 +6,16 @@
 //
 
 //
-// 뒤에서 5등까지
+// 조건에 맞게 수열 변환하기 3
 import Foundation
 
-func solution(_ num_list:[Int]) -> [Int] {
-    
-    var result = num_list
+func solution(_ arr:[Int], _ k:Int) -> [Int] {
 
-    result.sort()
+    let result = arr.map { k % 2 != 0 ? $0 * k : $0 + k}
 
-    return Array(result[0..<5])
+    return result
 }
 
-var result = solution([12, 4, 15, 46, 38, 1, 14])
+var result = solution([1, 2, 3, 100, 99, 98], 3)
 
 print(result)
