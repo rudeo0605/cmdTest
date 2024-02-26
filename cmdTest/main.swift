729fef8e669c35136c3f9543b5f2b8cf3efa85ce
KyungDae PARK <kyungdae.park@futurereality.io>
2024-02-26 13:43:23 +0900

    뒤에서 5등까지

--- cmdTest/main.swift
+++ cmdTest/main.swift
@@ -6,30 +6,18 @@
 //
 
 //
-// 조건에 맞게 수열 변환하기 1
+// 뒤에서 5등까지
 import Foundation
 
-func solution(_ arr:[Int]) -> [Int] {
+func solution(_ num_list:[Int]) -> [Int] {
+    
+    var result = num_list
 
-    let result : [Int] = arr.reduce(into:[]){ result, num in 
-            if(num >= 50 && num % 2 == 0)
-            {
-                result.append(num / 2)
-            }
-            else if( num < 50 && num % 2 == 1)
-            {
-                result.append(num * 2)
-            }
-            else
-            {
-                result.append(num)
-            }
-        
-    }
+    result.sort()
 
-    return result
+    return Array(result[0..<5])
 }
 
-var result = solution([1, 2, 3, 100, 99, 98])
+var result = solution([12, 4, 15, 46, 38, 1, 14])
 
 print(result)
