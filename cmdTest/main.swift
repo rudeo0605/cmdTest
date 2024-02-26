d6415fc3715c289b6e8ce1e43286aaf13aa03c2f
KyungDae PARK <kyungdae.park@futurereality.io>
2024-02-26 11:58:53 +0900

    순서쌍의 개수

--- cmdTest/main.swift
+++ cmdTest/main.swift
@@ -6,20 +6,31 @@
 //
 
 //
-// 중앙값 구하기
+// 순서쌍의 개수
 
 
 import Foundation
 
-func solution(_ array:[Int]) -> Int {
-
-    var sortedArray = array 
-    let centerIndex = array.count / 2
-
-    sortedArray.sort(by: <)
+func solution(_ n:Int) -> Int {
+    var result = 0
     
-    return sortedArray[centerIndex]
+    let sqrtN = Int(Double(n).squareRoot()) // n의 제곱근을 구합니다.
+    print(sqrtN)
+    for i in 1...sqrtN {
+        if n % i == 0 { // i가 n의 약수인 경우
+            let j = n / i
+            if i == j {
+                result += 1 // i와 j가 같으면, 즉 n이 완전제곱수인 경우
+            } else {
+                result += 2 // i와 j가 다른 두 개의 약수를 찾은 경우
+            }
+        }
+    }
+    return result
 }
-var result = solution([1, 2, 7, 10, 11])
+
+var result = solution(20)
+var result1 = solution(100)
 
 print(result)
+print(result1)
