6cdc6172312ce4e1cf228076e7650afea8bb8734
KyungDae PARK <kyungdae.park@futurereality.io>
2024-02-26 12:56:03 +0900

    정수 부분

--- cmdTest/main.swift
+++ cmdTest/main.swift
@@ -6,25 +6,13 @@
 //
 
 //
-// 숨어있는 숫자의 덧셈 (1)
+// 정수 부분
 
 import Foundation
 
-func solution(_ my_string:String) -> Int {
-
-    var result = 0;
-    let intLists:[Character] = ["0","1","2","3","4","5","6","7","8","9"]
-    for cha in my_string
-    {
-        if(intLists.contains(cha))
-        {
-            result += Int(String(cha))!
-        }
-    }
-    return result
+func solution(_ flo:Double) -> Int {
+    return Int(flo)
 }
 
-var result = solution("aAb1B2cC34oOp")
-var result1 = solution("1a2b3c4d123")
+var result = solution(1.42)
 print(result)
-print(result1)
