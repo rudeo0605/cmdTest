17d1e79441c2465900dabadb25f654e5bdfc58bd
KyungDae PARK <kyungdae.park@futurereality.io>
2024-02-26 13:38:54 +0900

    조건에 맞게 수열 변환하기 1

--- cmdTest/main.swift
+++ cmdTest/main.swift
@@ -6,13 +6,30 @@
 //
 
 //
-// n 번째 원소까지
+// 조건에 맞게 수열 변환하기 1
 import Foundation
 
-func solution(_ num_list:[Int], _ n:Int) -> [Int] {
-    return Array(num_list[0..<n])
+func solution(_ arr:[Int]) -> [Int] {
+
+    let result : [Int] = arr.reduce(into:[]){ result, num in 
+            if(num >= 50 && num % 2 == 0)
+            {
+                result.append(num / 2)
+            }
+            else if( num < 50 && num % 2 == 1)
+            {
+                result.append(num * 2)
+            }
+            else
+            {
+                result.append(num)
+            }
+        
+    }
+
+    return result
 }
 
-var result = solution([2, 1, 6], 1)
+var result = solution([1, 2, 3, 100, 99, 98])
 
 print(result)
